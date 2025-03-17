8d9bc4ff48d13069df6d02c8cd6603562f6c3ede
itzaditya57 <adityasinha07072009@gmail.com>
2025-03-18 00:17:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("message").innerText = "You shouldn't have clicked the button, Now you will have to give an Explosive Blowjob to Aditya Don💀";
+}
